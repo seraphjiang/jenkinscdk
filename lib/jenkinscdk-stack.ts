268cdda961e3531a4b0183b233090cdf1d57a733
Sichen Dong <sichend@amazon.com>
2021-11-12 14:48:43 -0800

    Refactor to break down functions

--- lib/jenkinscdk-stack.ts
+++ lib/jenkinscdk-stack.ts
@@ -5,32 +5,82 @@ import * as ec2 from "@aws-cdk/aws-ec2";
 import * as autoscaling from "@aws-cdk/aws-autoscaling";
 import * as elbv2 from "@aws-cdk/aws-elasticloadbalancingv2";
 import * as targets from "@aws-cdk/aws-route53-targets";
-import * as cfninc from '@aws-cdk/cloudformation-include';
-import * as iam from '@aws-cdk/aws-iam';
+import * as cfninc from "@aws-cdk/cloudformation-include";
+import * as iam from "@aws-cdk/aws-iam";
+import { SelectedSubnets } from "@aws-cdk/aws-ec2";
+import { ILoadBalancerV2 } from "@aws-cdk/aws-elasticloadbalancingv2";
 
-const SsmPolicyName = "service-role/AmazonEC2RoleforSSM"
-const NumWorkerNode = 2
+const SsmPolicyName = "service-role/AmazonEC2RoleforSSM";
+const DockerInstallationCmds = [
+  `runuser -l  ubuntu -c 'sudo systemctl stop apt-daily.timer'`,
+  `runuser -l  ubuntu -c 'sudo systemctl stop apt-daily-upgrade.timer'`,
+  `runuser -l  ubuntu -c 'sudo apt-get update'`,
+  `runuser -l  ubuntu -c 'sudo apt install -y docker.io'`,
+  `runuser -l  ubuntu -c 'sudo usermod -aG docker jenkins'`,
+  `runuser -l  ubuntu -c 'sudo systemctl start apt-daily.timer'`,
+  `runuser -l  ubuntu -c 'sudo systemctl start apt-daily-upgrade.timer'`,
+];
+const InstanceTypes = {
+  master: ec2.InstanceType.of(ec2.InstanceClass.M5, ec2.InstanceSize.XLARGE),
+  worker: ec2.InstanceType.of(ec2.InstanceClass.C5, ec2.InstanceSize.XLARGE4),
+};
+const Capacities = {
+  // Only support fixed number of host for now
+  master: 1,
+  worker: 2,
+};
 
 export class JenkinscdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: any) {
     super(scope, id, props);
 
+    const { hostedZone: domainName, ami, az, keyPair, region } = props.env;
+    const machineImage = ec2.MachineImage.genericLinux({ [region]: ami });
+    const zone = route53.HostedZone.fromLookup(this, "jenkins-dns", {
+      privateZone: false,
+      domainName,
+    });
+    const jenkinsDomain = `jenkins.${domainName}`;
+    const certificate = new cm.Certificate(this, "jenkins-dns-cert", {
+      domainName: jenkinsDomain,
+      validation: cm.CertificateValidation.fromDns(zone),
+    });
+
+    const { vpc, subnets } = this.createNetwork(az);
+
+    const masterAsg = this.createAsg(
+      vpc,
+      subnets,
+      InstanceTypes.master,
+      machineImage,
+      keyPair,
+      Capacities.master
+    );
+    const workerAsg = this.createAsg(
+      vpc,
+      subnets,
+      InstanceTypes.worker,
+      machineImage,
+      keyPair,
+      Capacities.worker
+    );
+
+    const lb = this.createLoadBalancer(masterAsg, vpc, certificate);
+
+    new route53.ARecord(this, "jenkins-dns-alias", {
+      recordName: jenkinsDomain,
+      target: route53.RecordTarget.fromAlias(
+        new targets.LoadBalancerTarget(lb)
+      ),
+      zone,
+    });
+
     new cfninc.CfnInclude(this, `PVREReportingTemplate-${id}`, {
       templateFile: "lib/pvre-reporting-template.yml",
     });
+  }
 
-    const { hostedZone: domainName, account, ami, az, keyPair, region } = props.env;
-    const zone = route53.HostedZone.fromLookup(this, "jenkins-dns", {
-      privateZone: false,
-      domainName
-    });
-
-    const jenkinsDomain = `jenkins.${domainName}`
-    const certificate = new cm.Certificate(this, "jenkins-dns-cert", {
-      domainName: jenkinsDomain,
-      validation: cm.CertificateValidation.fromDns(zone)
-    });
-
+  createNetwork(az: string): { vpc: ec2.IVpc; subnets: SelectedSubnets } {
     const vpc = new ec2.Vpc(this, "jenkins-vpc", {
       subnetConfiguration: [
         {
@@ -40,73 +90,55 @@ export class JenkinscdkStack extends cdk.Stack {
         },
       ],
     });
+    const subnets = vpc.selectSubnets({ availabilityZones: [az] });
+    return { vpc, subnets };
+  }
 
+  createAsg(
+    vpc: ec2.IVpc,
+    subnets: ec2.SelectedSubnets,
+    instanceType: ec2.InstanceType,
+    machineImage: ec2.IMachineImage,
+    keyPair: string,
+    capacity: number
+  ): autoscaling.AutoScalingGroup {
+    const masterAsg = new autoscaling.AutoScalingGroup(
+      this,
+      "jenkins-master-asg",
+      {
+        vpc,
+        instanceType,
+        machineImage,
+        vpcSubnets: subnets,
+        keyName: keyPair,
+        maxCapacity: capacity,
+        minCapacity: capacity,
+      }
+    );
     const ssmPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName(SsmPolicyName);
-    const machineImage = ec2.MachineImage.genericLinux({[region]: ami});
-    const subnets = vpc.selectSubnets({availabilityZones: [az]})
+    masterAsg.role.addManagedPolicy(ssmPolicy);
+    masterAsg.userData.addCommands(...DockerInstallationCmds);
+    return masterAsg;
+  }
 
-    const masterAsg = new autoscaling.AutoScalingGroup(this, 'jenkins-master-asg', {
+  createLoadBalancer(
+    asg: autoscaling.AutoScalingGroup,
+    vpc: ec2.IVpc,
+    certificate: cm.Certificate
+  ): ILoadBalancerV2 {
+    const lb = new elbv2.ApplicationLoadBalancer(this, "jenkins-master-lb", {
       vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.M5, ec2.InstanceSize.XLARGE),
-      machineImage: machineImage,
-      vpcSubnets: subnets,
-      keyName: keyPair,
+      internetFacing: true,
     });
-
-    masterAsg.role.addManagedPolicy(ssmPolicy)
-
-    const workerAsg = new autoscaling.AutoScalingGroup(this, 'jenkins-worker-asg', {
-      vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.C5, ec2.InstanceSize.XLARGE4),
-      machineImage: machineImage,
-      vpcSubnets: subnets,
-      keyName: keyPair,
-      maxCapacity: NumWorkerNode,
-      minCapacity: NumWorkerNode,
-    });
-
-    workerAsg.role.addManagedPolicy(ssmPolicy)
-  
-    const initCmds = [
-      `runuser -l  ubuntu -c 'sudo systemctl stop apt-daily.timer'`,
-      `runuser -l  ubuntu -c 'sudo systemctl stop apt-daily-upgrade.timer'`,
-      `runuser -l  ubuntu -c 'sudo apt-get update'`,
-      `runuser -l  ubuntu -c 'sudo apt install -y docker.io'`,
-      `runuser -l  ubuntu -c 'sudo usermod -aG docker jenkins'`,
-      `runuser -l  ubuntu -c 'sudo systemctl start apt-daily.timer'`,
-      `runuser -l  ubuntu -c 'sudo systemctl start apt-daily-upgrade.timer'`,
-    ];
-
-    masterAsg.userData.addCommands(...initCmds);
-    workerAsg.userData.addCommands(...initCmds);
-
-    const lb = new elbv2.ApplicationLoadBalancer(this, 'jenkins-master-lb', {
-      vpc,
-      internetFacing: true
-    });
-
-    const listener = lb.addListener('Listener', {
+    const listener = lb.addListener("Listener", {
       port: 443,
       certificateArns: [certificate.certificateArn],
     });
-
-    listener.addTargets('Target', {
+    listener.addTargets("Target", {
       port: 8080,
-      targets: [masterAsg]
+      targets: [asg],
     });
-
-    listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
-
-    masterAsg.scaleOnRequestCount('AModestLoad', {
-      targetRequestsPerSecond: 1
-    });
-
-    new route53.ARecord(this, "jenkins-dns-alias", {
-      recordName: jenkinsDomain,
-      target: route53.RecordTarget.fromAlias(
-        new targets.LoadBalancerTarget(lb)
-      ),
-      zone,
-    });
+    listener.connections.allowDefaultPortFromAnyIpv4("Open to the world");
+    return lb;
   }
 }
